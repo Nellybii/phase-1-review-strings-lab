17543405464e68bd5531220519f2cc6be51957a1
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-09-18 13:47:51 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+let firstInitial = currentUser[0];
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
